17cd764dbd7451a7b69c36d1ef7eb53b35ebcde6
thapa technical <vbthapa55@gmail.com>
2024-06-19 17:11:10 +0530

    react day 30 - derived state in react js

--- src/components/DericedState.jsx
+++ src/components/DericedState.jsx
@@ -5,7 +5,15 @@ export const DerivedState = () => {
     { name: "Alice", age: 25 },
     { name: "Bob", age: 30 },
     { name: "Charlie", age: 35 },
+    { name: "Angles", age: 45 },
   ]);
+
+  // Derived state: count of users
+  console.log(users);
+  const userCount = users.length;
+  // Derived state: average age of users
+  const averageAge =
+    users.reduce((accum, curElem) => accum + curElem.age, 0) / userCount;
 
   return (
     <div className="main-div">
@@ -17,6 +25,8 @@ export const DerivedState = () => {
           </li>
         ))}
       </ul>
+      <p>Total Users: {userCount}</p>
+      <p>Average Age: {averageAge}</p>
     </div>
   );
 };
